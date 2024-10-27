bbf9e05f4ec622fd484335c3820f903192305115
octvs <orhuncenktaskin@gmail.com>
2024-10-27 23:48:40 +0100

    feat: side by side w/ input and translation

--- src/translate.py
+++ src/translate.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import re
 import subprocess
 import sys
@@ -6,6 +7,25 @@ import sys
 import deepl
 
 languages = {"de": "DE", "en": "EN-US", "tr": "TR"}
+
+
+def side_by_side(strings, terminal_w=os.get_terminal_size().columns, gap=4):
+    w = (terminal_w - gap - 1) // 2
+    separator = " " * (gap // 2) + "|" + " " * (gap // 2)
+    result = []
+    while any(strings):
+        line = []
+        for i, s in enumerate(strings):
+            buf = s[:w]
+            try:
+                n = buf.index("\n")
+                line.append(buf[:n].ljust(w))
+                strings[i] = s[n + 1 :]
+            except ValueError:
+                line.append(buf.ljust(w))
+                strings[i] = s[w:]
+        result.append(separator.join(line))
+    return "\n".join(result)
 
 
 def main():
@@ -36,13 +56,12 @@ def main():
     ).group(1)
     translator = deepl.Translator(auth_key)
     input = sys.stdin.read()
-    print(
-        translator.translate_text(
-            input,
-            source_lang=languages[args._from],
-            target_lang=languages[args._to],
-        )
+    result = translator.translate_text(
+        input,
+        source_lang=languages[args._from],
+        target_lang=languages[args._to],
     )
+    print(side_by_side([input, result.text]))
 
 
 if __name__ == "__main__":
